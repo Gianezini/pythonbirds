d62c4f7a122d239bd9535f7d339df1c447f73c6e
mauricio.PythonBirds <mauriciogianezini@hotmail.com>
2020-07-24 16:04:06 -0300

    Criado atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=Non, idade=35):
+        self.idade = idade
+        self.nome = None
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Mauricio')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Renzo'
+    print(p.nome)
+    print(p.idade)
